7b379ee0bf922e955af6e1d7a7f2208b8cd89265
Maria Mills <cxwinterx@gmail.com>
2023-11-19 23:44:20 -0500

    Add /facts route with optional 'random' query parameter for random fact retrieval

---
+++ src/app/api/facts/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from "next/server";
+import christmasFacts from "../../../data/christmasFacts.json";
+
+export function GET(request: NextRequest) {
+  // Get a random fact if query contains random
+    if (request.url.includes('random')) {
+        const randomFact = christmasFacts[Math.floor(Math.random() * christmasFacts.length)];
+        return NextResponse.json({ fact: randomFact });
+    }
+
+    // Return all facts
+    return NextResponse.json(christmasFacts);
+}
