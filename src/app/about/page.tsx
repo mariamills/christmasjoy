ef4d3c4a86b8195d0c6c8bba20b3fd47c1c2b905
Maria Mills <cxwinterx@gmail.com>
2023-12-02 20:54:49 -0500

    Add placeholder pages and established /api route for future use.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>About</h1>
+        <h1>About</h1>
+        <p>This page is currently under construction.</p>
     </main>
   )
 }
---
+++ src/app/docs/examples/page.tsx
@@ -0,0 +1,10 @@
+function Examples() {
+    return (
+        <div>
+        <h1>Examples</h1>
+        <p>This page is currently under construction.</p>
+        </div>
+    );
+}
+
+export default Examples;
