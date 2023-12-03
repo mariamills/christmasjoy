08b7f6fa9eb847c43ca0408e95991033ade46d58
Maria Mills <cxwinterx@gmail.com>
2023-12-02 20:29:02 -0500

    Implement countdown to Christmas API endpoint with detailed time metrics

---
+++ src/app/api/countdown/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse, NextRequest } from "next/server";
+
+enum DayOfWeek {
+    Sunday = 0,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+}
+
+function getDayOfWeek(day: DayOfWeek): string {
+    return DayOfWeek[day];
+}
+
+interface TimeUntilChristmas {
+    months: number;
+    weeks: number;
+    sleeps: number;
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+    dayOfTheWeek: string;
+}
+
+function getTimeUntilChristmas(): TimeUntilChristmas {
+    const now = new Date();
+    const year = now.getMonth() === 11 && now.getDate() >= 25 ? now.getFullYear() + 1 : now.getFullYear();
+
+    const christmas = new Date(year, 11, 25); // Month is 0-indexed, 11 = December
+    const diff = christmas.getTime() - now.getTime(); // Difference in milliseconds
+
+    const days = diff / (1000 * 60 * 60 * 24);
+    const weeks = days / 7;
+    const months = days / 30; // Approximation
+
+    return {
+        months: Math.floor(months),
+        weeks: Math.floor(weeks),
+        sleeps: Math.ceil(days),
+        days: Math.floor(days),
+        hours: Math.floor(days * 24),
+        minutes: Math.floor(days * 24 * 60),
+        seconds: Math.floor(days * 24 * 60 * 60),
+        dayOfTheWeek: getDayOfWeek(christmas.getDay()),
+    };
+}
+
+export function GET(request: NextRequest) {
+    return NextResponse.json(getTimeUntilChristmas());
+}
