eec8586a7b728025ed61ce522eda1053f8607bee
Maria Mills <cxwinterx@gmail.com>
2023-12-02 21:18:14 -0500

    Remove DocsHeader.tsx for now

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,4 +1,3 @@
-import DocsHeader from '@/components/DocsHeader'
 import DocsSidebar from "@/components/DocsSidebar";
 
 export default function DocsLayout({
@@ -8,8 +7,6 @@ export default function DocsLayout({
 }>) {
     return (
         <section>
-            {/* Disabling DocsHeader for now.. TODO: fix this */}
-            {/*<DocsHeader />*/}
             < DocsSidebar />
             <main className="mt-10 lg:ml-80 lg:mx-72 md:mx-12 sm:ml-28 p-4 md:p-0.5">{children}</main>
         </section>
