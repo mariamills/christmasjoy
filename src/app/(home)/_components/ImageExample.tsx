c0364d43eaf55f9e89f3a82fd117e705d1843cf5
Maria Mills <cxwinterx@gmail.com>
2024-12-09 09:08:58 -0500

    Add additional margin for mobile styling

--- src/app/(home)/_components/ImageExample.tsx
+++ src/app/(home)/_components/ImageExample.tsx
@@ -34,7 +34,7 @@ function ChristmasImage() {
                         <Link
                             href="/docs/endpoints"
                             type="button"
-                            className="rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 mt-4"
+                            className="rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 my-4"
                         >Learn More
                         </Link>
                     </div>
